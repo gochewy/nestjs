703ba10ce71ec9a934fdd53b93393a4dff2a505e
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2021-01-04 00:58:08 -0800

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import {Controller, Get, Inject, OnApplicationBootstrap, Query,} from '@nestjs/common';
+import {ClientProxy} from '@nestjs/microservices';
+
+@Controller()
+export class AppController implements OnApplicationBootstrap {
+  constructor(@Inject('TASKS_SERVICE') private client: ClientProxy) {}
+
+  async onApplicationBootstrap() {
+    await this.client.connect();
+  }
+
+  @Get()
+  async getHello(@Query('greeting') greeting?: string) {
+    this.client.emit('hello', { value: greeting || 'hello' });
+
+    return 'potato';
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'TASKS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://user:bitnami@localhost:5672/'],
+          queue: 'tasks',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
