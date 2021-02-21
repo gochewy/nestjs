ad17b91ce40d88e56266ec4d18801d1b041c09e6
swarn <swarnchahal@Swarns-MacBook-Pro.local>
2021-02-21 12:00:27 -0500

    apollo client setup

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,10 +1,38 @@
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import ApolloClient, { InMemoryCache, HttpLink, split } from 'apollo-boost';
 import fetch from 'cross-fetch';
-const httpEndpont = 'http://localhost:5000/v1/graphql';
+import { WebSocketLink } from 'apollo-link-ws';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { store } from '../../../client/src/store';
+const httpEndpoint = 'http://localhost:5000/v1/graphql';
 
 const createClient = (token) => {
+  const httpLink = new HttpLink({
+    uri: httpEndpoint,
+    fetch,
+    headers: {
+      'x-hasura-admin-secret': 'password',
+    },
+  });
+  const wsEndpoint = `ws${httpEndpoint?.slice(4)}`;
+
+  const subscriptionClient = new SubscriptionClient(
+    `${wsEndpoint}/v1/graphql`,
+    {
+      reconnect: true,
+      connectionParams: () => {
+        const { token } = store.getState().auth;
+        return {
+          headers: {
+            authorization: token ? `Bearer ${token}` : '',
+          },
+        };
+      },
+    },
+  );
+  const wsLink = new WebSocketLink(subscriptionClient);
+  const link = split();
   return new ApolloClient({
-    uri: httpEndpont,
+    uri: httpEndpoint,
     fetch,
     headers: {
       'x-hasura-admin-secret': 'password',
